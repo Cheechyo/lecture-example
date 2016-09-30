40dc96eaf12159314902dde10f5c3f5d18d14640
Cheechyo <kwj9211@gmail.com>
2016-09-30 17:42:29 +0900

    Junit 코드 작성
    Junit4 코드 작성
    Mock 코드 작성.... File은 외부 요인이 많아서 힘듬.
    * Junit의 라이브러리가 왜인지 없기때문에 주석처리함.

---
+++ src/main/java/daumtrack/oop2016/WatcherTest.java
@@ -0,0 +1,59 @@
+package daumtrack.oop2016;
+
+import java.io.File;
+
+//import static org.junit.Assert.*;
+
+/**
+ * Created by Cheechyo on 2016. 9. 30..
+ */
+public class WatcherTest {
+    /*
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void watch() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void deleted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void modified() throws Exception {
+
+    }
+
+    @Test
+    public void test_1() throws Exception {
+        // Given
+        File path = new File(".");
+        long lastChecked = 1111;
+        // When
+        Set<File> result = sut.getModified(path, lastChecked);
+        // Then
+        assertThat()
+    }
+    @Test
+    public void test_mock() throws Exception {
+        // Given
+        File mockfile = new mock(File.class);
+        when(mockfile.isDirectory()).thenReturn(true);
+
+        // When
+        Set<File> result = sut.getModified(mockfile, 000);
+        // Then
+        assertThat(result.size(), is(0));
+    }
+    */
+}
